35ade87c76924b404ac4583365b8dc69edcbbc7d
Fredrik Bendiksen <fbendiksen@gmail.com>
2022-09-07 17:28:43 +0200

    Created Assignment 2

---
+++ Oblig 2 - Kunst/functions.js
@@ -0,0 +1,18 @@
+function FillPolygon (xCoords, yCoords){
+    ctx.beginPath();
+    ctx.moveTo(xCoords[0], yCoords[0]);
+    for (let i = 1; i < xCoords.length; i++){
+        ctx.lineTo(xCoords[i], yCoords[i]);
+    }
+    ctx.closePath();
+    ctx.fill();
+}
+function ConnectDots (xCoords, yCoords){
+    ctx.beginPath();
+    ctx.moveTo(xCoords[0], yCoords[0]);
+    for (let i = 1; i < xCoords.length; i++){
+        ctx.lineTo(xCoords[i], yCoords[i]);
+    }
+    ctx.closePath();
+    ctx.stroke();
+}
